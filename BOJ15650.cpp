b8571b8c3c930b75a056af04c2ffff50b8f4cf40
lolmc <illionaire1004@gmail.com>
2019-10-01 12:28:52 +0900

    재귀

--- BOJ15650.cpp
+++ BOJ15650.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main(){
+int N, M;
 
+void printBit(int bit){
+    for(int i = 0; i < N; i++){
+        if(bit & (1 << i))
+            printf("%d ", i + 1);
+    }
+    putchar('\n');
+}
+
+void process(int bit, int idx, int cnt){
+    if(cnt == M){
+        printBit(bit);
+        return;
+    }
+    for(int i = idx + 1; i <= N; i++){
+        process(bit | (1 << (i - 1)), i, cnt+1);
+    }
+
+}
+
+int main(){
+    scanf("%d %d", &N, &M);
+    for(int i = 1; i <= N; i++){
+        process(1 << (i - 1), i, 1);
+    }
     return 0;
 }
