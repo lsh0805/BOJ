9bb506c1731020a22a5ad034fede2be692ec3467
lolmc00 <lolmc0805@gmail.com>
2020-04-07 23:10:18 +0900

    전국 대학생 프로그래밍 대회 동아리 연합 여름 대회 2018

---
+++ solved/BOJ15890.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+typedef long long ll;
+typedef pair<int, int> p;
+typedef pair<int, pair<int, int>> p2;
+typedef vector<int> vi;
+
+int T, N, ans;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    cout << "20180729";
+    return 0;
+}
