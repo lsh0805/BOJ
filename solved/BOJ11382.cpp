bb05508ed62695f99b2da412e3168be72e4dcf52
lolmc00 <lolmc0805@gmail.com>
2020-04-08 09:07:10 +0900

    꼬마 정민

---
+++ solved/BOJ11382.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+typedef long long ll;
+typedef pair<int, int> p;
+typedef pair<int, pair<int, int>> p2;
+typedef vector<int> vi;
+
+int T, N, ans;
+long long A, B, C;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin >> A >> B >> C;
+    cout << A + B + C;
+    return 0;
+}
