af38373548d035fdd7f443ca2b3111f4d77ed996
lolmc <illionaire1004@gmail.com>
2020-11-20 22:28:49 +0900

    두 배열의 합

--- solved/BOJ2143.cpp
+++ solved/BOJ2143.cpp
@@ -2,41 +2,44 @@
 
 using namespace std;
 
-#define mp make_pair
-#define pb push_back
-#define fi first
-#define se second
 typedef long long ll;
-typedef vector<int> vi;
-map<ll, ll> m;
-ll T, N, M, a[1001], b[1001], a_sum, b_sum, ans;
 
-int main()
-{
-    cin.tie(0);
-    ios::sync_with_stdio(false);
+int T, n, m, A[1001], B[1001], a_sum[1001], b_sum[1001];
+ll ans;
+vector<int> a_seg, b_seg;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> T;
-    cin >> N;
-    for(int i = 0; i < N; i++)
-        cin >> a[i];
-    for(int i = 0; i < N; i++){
-        a_sum = 0;
-        for(int j = i; j < N; j++){
-            a_sum += a[j];
-            m[a_sum]++;
-        }
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> A[i];
+        if(i > 0)
+            a_sum[i] = A[i] + a_sum[i - 1];
+        else
+            a_sum[i] = A[i];
+        a_seg.push_back(a_sum[i]);
     }
-    cin >> M;
-    for (int i = 0; i < M; i++)
-        cin >> b[i];
-    
-    for(int i = 0; i < M; i++){
-        b_sum = 0;
-        for(int j = i; j < M; j++){
-            b_sum += b[j];
-            ans += m[T - b_sum];
-        }
+    cin >> m;
+    for(int i = 0; i < m; i++){
+        cin >> B[i];
+        if(i > 0)
+            b_sum[i] = B[i] + b_sum[i - 1];
+        else
+            b_sum[i] = B[i];
+        b_seg.push_back(b_sum[i]);
     }
-    cout << ans << '\n';
-    return 0;
+    for(int i = 1; i < m; i++)
+        for(int j = i; j < m; j++)
+            b_seg.push_back(b_sum[j] - b_sum[i - 1]);
+    sort(b_seg.begin(), b_seg.end());
+    for(int i = 1; i < n; i++)
+        for(int j = i; j < n; j++)
+            a_seg.push_back(a_sum[j] - a_sum[i - 1]);
+    for(int i = 0; i < a_seg.size(); i++){
+        auto it = lower_bound(b_seg.begin(), b_seg.end(), T - a_seg[i]);
+        if(it == b_seg.end() || *it != T - a_seg[i]) continue;
+        ans += upper_bound(b_seg.begin(), b_seg.end(), T - a_seg[i]) - it;
+    }
+    cout << ans;
 }
