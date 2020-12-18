297db67d8dd10142dd006f4093ccc4420c019f17
lolmc <illionaire1004@gmail.com>
2020-12-18 21:59:39 +0900

    A+B

---
+++ solved/BOJ1000.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<int, pair<int, int>> P2;
+
+int A, B;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> A >> B;
+    cout << A + B;
+}
