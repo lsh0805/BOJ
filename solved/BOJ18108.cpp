c9b69765e7415528dc03b355e5b5113d0a889500
lolmc <lolmc080@gmail.com>
2022-02-04 22:58:13 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ solved/BOJ18108.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IO                     \
+  ios::sync_with_stdio(false); \
+  cin.tie(0);                  \
+  cout.tie(0);
+#define LL long long
+#define endl "\n"
+
+typedef pair<int, int> ii;
+
+int main()
+{
+  IO;
+  int Y;
+  int diff = 2541 - 1998;
+  cin >> Y;
+  cout << Y - diff << endl;
+  return 0;
+}
