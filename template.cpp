380603bfa68258a37d7ad8729943f302291495c5
lolmc <lolmc080@gmail.com>
2022-02-04 22:59:13 +0900

    template 추가

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+#define LL long long
+#define endl "\n"
+
+typedef pair<int, int> ii;
+
+using namespace std;
+
+int main()
+{
+    IO;
+
+    return 0;
+}
