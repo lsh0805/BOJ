6ab50da4d1d2a8200fb8b2666c0da4fdff8909d3
lolmc00 <lolmc0805@gmail.com>
2022-03-08 17:29:35 +0900

    占쏙옙

---
+++ solved/BOJ11506.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+#define ll long long
+#define endl "\n"
+#define fi first
+#define se second
+typedef pair<int, int> pii;
+
+int main()
+{
+    IO;
+    cout << "�";
+    return 0;
+}
