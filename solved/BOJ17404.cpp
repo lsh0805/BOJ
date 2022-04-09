3b0039a7cfd2fa0779b2bd97232b45248c08b9db
lolmc00 <lolmc0805@gmail.com>
2022-04-09 23:32:50 +0900

    RGB거리 2

--- solved/BOJ17404.cpp
+++ solved/BOJ17404.cpp
@@ -5,8 +5,7 @@ using namespace std;
 template <typename A, typename B>
 ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
-ostream &operator<<(ostream &os, const T_container &v)
-{
+ostream &operator<<(ostream &os, const T_container &v) {
     os << '{';
     string sep;
     for (const T &x : v)
@@ -15,8 +14,7 @@ ostream &operator<<(ostream &os, const T_container &v)
 }
 void dbg_out() { cerr << endl; }
 template <typename Head, typename... Tail>
-void dbg_out(Head H, Tail... T)
-{
+void dbg_out(Head H, Tail... T) {
     cerr << ' ' << H;
     dbg_out(T...);
 }
@@ -42,19 +40,40 @@ const ll MOD1 = 998'244'353;
 const ll INF = 1e9;
 const ld EPS = 1e-9;
 
-void solve()
-{
+int N, dp[1001][3][3], c[1001][3];
+
+void solve() {
+    cin >> N;
+    int ans = 1e9;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> c[i][j];
+            for (int k = 0; k < 3; k++) {
+                dp[i][j][k] = 1e9;
+            }
+        }
+    }
+    for (int k = 0; k < 3; k++) {
+        dp[0][k][k] = c[0][k];
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == N - 1 && j == k) continue;
+                dp[i][j][k] = min(dp[i - 1][(j + 1) % 3][k], dp[i - 1][(j + 2) % 3][k]) + c[i][j];
+                if (i == N - 1)
+                    ans = min(ans, dp[i][j][k]);
+            }
+        }
+    }
+    cout << ans << "\n";
 }
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int tc = 1;
     // cin >> tc;
-    for (int t = 1; t <= tc; t++)
-    {
+    for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t << ": ";
         solve();
     }
