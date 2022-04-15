41bb0dd28367a11a6729cfc5478e3aaeaedcfae9
lolmc00 <lolmc0805@gmail.com>
2022-04-15 14:06:58 +0900

    CCW

---
+++ solved/BOJ11758.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v) {
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fi first
+#define se second
+
+typedef pair<int, int> pii;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll MOD1 = 998'244'353;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int P[3][2];
+
+void solve() {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 2; j++)
+            cin >> P[i][j];
+    pii a = {P[1][0] - P[0][0], P[1][1] - P[0][1]};
+    pii b = {P[2][0] - P[1][0], P[2][1] - P[1][1]};
+    int ccw = a.fi * b.se - a.se * b.fi;
+    if (ccw > 0) {
+        cout << "1";
+    } else if (ccw == 0) {
+        cout << "0";
+    } else {
+        cout << "-1";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
