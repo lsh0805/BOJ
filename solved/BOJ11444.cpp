e6e50b34bfdf3178426099ed554704636abc8feb
lolmc00 <lolmc0805@gmail.com>
2022-04-15 23:43:30 +0900

    피보나치 수 6

---
+++ solved/BOJ11444.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v) {
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fi first
+#define se second
+
+typedef pair<int, int> pii;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll MOD1 = 998'244'353;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+ll N;
+
+vector<vector<ll>> mul(const vector<vector<ll>> &a, const vector<vector<ll>> &b) {
+    vector<vector<ll>> ret(2, vector<ll>(2));
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                ret[i][j] += a[i][k] * b[k][j];
+                ret[i][j] %= MOD;
+            }
+        }
+    }
+    return ret;
+}
+
+void solve() {
+    cin >> N;
+    vector<vector<ll>> A = {{1, 1}, {1, 0}};
+    if (N == 0) {
+        cout << '0';
+    } else if (N <= 2) {
+        cout << '1';
+    } else {
+        N -= 2;
+        vector<vector<ll>> A = {{1, 1}, {1, 0}};
+        vector<vector<ll>> a = {{1, 1}, {1, 0}};
+        while (N) {
+            if (N & 1) a = mul(a, A);
+            A = mul(A, A);
+            N /= 2;
+        }
+        dbg(a);
+        cout << a[0][0];
+    }
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
