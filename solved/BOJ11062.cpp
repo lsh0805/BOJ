c20f60eeaf6ea8e1499b563b2e74b9f94977b40d
lolmc00 <lolmc0805@gmail.com>
2022-04-27 09:16:37 +0900

    카드 게임

---
+++ solved/BOJ11062.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v) {
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fi first
+#define se second
+
+typedef pair<int, int> pii;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll MOD1 = 998'244'353;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int N, a[1001], dp[1001][1001];
+vector<int> v;
+
+int f(int l, int r) {
+    if (l > r || l < 0) return 0;
+    int turn = (N - (l - r + 1)) % 2;
+    int &ret = dp[l][r];
+    if (ret != 0) return ret;
+    int lm = min(f(l + 2, r), f(l + 1, r - 1));
+    int rm = min(f(l, r - 2), f(l + 1, r - 1));
+    return ret = max(lm + a[l], rm + a[r]);
+}
+
+void solve() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            dp[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    cout << f(0, N - 1) << '\n';
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
