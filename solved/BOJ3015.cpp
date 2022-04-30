5a3b17c3e3062686157bb73d0f2148b77514be3c
lolmc00 <lolmc0805@gmail.com>
2022-04-30 13:17:17 +0900

    오아시스 재결합

---
+++ solved/BOJ3015.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v) {
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fi first
+#define se second
+
+typedef pair<int, ll> pii;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll MOD1 = 998'244'353;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int N;
+
+ll cal(vector<int> a, bool r) {
+    ll ret = 0;
+    stack<pii> st;
+    for (int i = 0; i < a.size(); i++) {
+        while (!st.empty() && st.top().fi < a[i]) {
+            ret += st.top().se;
+            dbg(i, st.top().se);
+            st.pop();
+        }
+        if (!st.empty() && st.top().fi == a[i]) {
+            if (!r)
+                ret += st.top().se;
+            dbg(i, st.top().se);
+            st.top().se++;
+        } else {
+            st.push({a[i], 1});
+        }
+    }
+    return ret;
+}
+
+void solve() {
+    cin >> N;
+    vector<int> a(N);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    ll ans = cal(a, false);
+    reverse(a.begin(), a.end());
+    ans += cal(a, true);
+    cout << ans;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
