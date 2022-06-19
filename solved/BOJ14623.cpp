04487beb65aabe17c3f353fe2267bc3abbfd973a
lolmc00 <lolmc0805@gmail.com>
2022-06-19 22:50:49 +0900

    감정이입

---
+++ solved/BOJ14623.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v) {
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fi first
+#define se second
+
+typedef pair<int, int> pii;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll MOD1 = 998'244'353;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+string a, b;
+
+ll b_to_d(string b) {
+    ll d = 0;
+    for (int i = 0; i < b.length(); i++) {
+        int bit = b[b.length() - i - 1] - '0';
+        if (bit) {
+            d += (1 << i);
+        }
+    }
+    return d;
+}
+string d_to_b(ll d) {
+    string b = "";
+    while (d != 0) {
+        if (d % 2) {
+            b += "1";
+        } else {
+            b += "0";
+        }
+        d >>= 1;
+    }
+    reverse(b.begin(), b.end());
+    return b;
+}
+void solve() {
+    cin >> a >> b;
+    cout << d_to_b(b_to_d(a) * b_to_d(b));
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
