b12c5d37a5aef43ee565e85451030f0087992e56
lolmc00 <lolmc0805@gmail.com>
2022-06-19 16:00:29 +0900

    Dongjak N1

---
+++ solved/BOJ14566.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v) {
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fi first
+#define se second
+
+typedef pair<int, int> pii;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll MOD1 = 998'244'353;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int N, a[5001];
+
+void solve() {
+    cin >> N;
+    int cnt = 0, min_ = 2e9;
+    for (int i = 0; i < N; i++)
+        cin >> a[i];
+    sort(a, a + N);
+    for (int i = 0; i < N - 1; i++) {
+        int diff = a[i + 1] - a[i];
+        if (diff < min_) {
+            cnt = 1;
+            min_ = diff;
+        } else if (diff == min_) {
+            cnt++;
+        }
+    }
+    cout << min_ << " " << cnt;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
